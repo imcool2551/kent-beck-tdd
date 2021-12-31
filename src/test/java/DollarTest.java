ad2e7c864778ce48ea291e6a84a43d67c8aae7a7
SangWook <imcool2551@gmail.com>
2021-12-31 19:13:04 +0900

    chapter ~6

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -19,8 +19,10 @@ public class DollarTest {
     }
 
     @Test
-    public void testEqaulity() {
+    public void testEquity() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
     }
 }
